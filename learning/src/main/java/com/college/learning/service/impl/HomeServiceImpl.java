43c02c38b85b465d1915e8ec913d76ddc47d7136
manish kumar <manish.kr.swe>
2023-07-29 17:34:55 +0530

    created homeserviceimpl class

---
+++ learning/src/main/java/com/college/learning/service/impl/HomeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.college.learning.service.impl;
+
+import com.college.learning.service.HomeService;
+
+public class HomeServiceImpl implements HomeService {
+
+	@Override
+	public String getHome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
