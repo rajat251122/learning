2b3ed689882a0d88c2281a13b4296c039894fffe
p <p@192.168.0.8>
2024-07-16 00:04:56 +0530

    updated endpoint

--- learning/src/main/java/com/college/learning/controller/StudentController.java
+++ learning/src/main/java/com/college/learning/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.college.learning.entity.Student;
@@ -19,7 +20,7 @@ public class StudentController {
 	@Autowired
 	StudentService studentService;
 	
-	 @GetMapping("/student-details")
+	 @GetMapping("/student-detail")
 	    public List<Student> getStudentDetails() {
 	        return studentService.getStudentDetails();
 	    }
